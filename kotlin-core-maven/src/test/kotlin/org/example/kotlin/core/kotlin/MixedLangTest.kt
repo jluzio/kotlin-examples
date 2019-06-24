e0ce4008e0b4689c1b5dc4e8bb03340850cbfb78
João Luzio <joao.luzio@gmail.com>
2019-06-24 13:14:30 +0100

    Testing Java 6 with Kotlin Lambdas

--- kotlin-core-maven/src/test/kotlin/org/example/kotlin/core/kotlin/MixedLangTest.kt
+++ kotlin-core-maven/src/test/kotlin/org/example/kotlin/core/kotlin/MixedLangTest.kt
@@ -8,5 +8,7 @@ class MixedLangTest {
     fun test() {
         println(org.example.kotlin.core.kotlin.model.BarBean())
         println(org.example.kotlin.core.java.model.FooBean())
+
+        listOf(1, 2, 3).forEach { v -> println("number: $v" ) }
     }
 }
