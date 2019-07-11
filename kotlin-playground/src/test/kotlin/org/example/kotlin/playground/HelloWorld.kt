e164cb1606404001ed21fd2a45880871a1942c92
João Luzio <joao.luzio@gmail.com>
2019-07-11 16:08:28 +0100

    Adding examples

---
+++ kotlin-playground/src/test/kotlin/org/example/kotlin/playground/HelloWorld.kt
@@ -0,0 +1,5 @@
+package org.example.kotlin.playground
+
+fun main() {
+    println("Hello World!")
+}
--- kotlin-playground/src/test/kotlin/org/example/kotlin/playground/feature_core/HelloWorld.kt
+++
@@ -1,7 +0,0 @@
-package org.example.kotlin.playground.feature_core
-
-class HelloWorld
-
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
