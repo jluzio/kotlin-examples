4a223b9e0d8e33cdb98f8424d5335ffbd27f5622
João Luzio <joao.luzio@gmail.com>
2019-11-19 19:38:21 +0000

    Kotlin Course

--- kotlin-course-201911/src/com/example/kotlin/course/interfaces/Interfaces.kt
+++ kotlin-course-201911/src/com/example/kotlin/course/interfaces/Interfaces.kt
@@ -1,7 +1,15 @@
 package com.example.kotlin.course.interfaces
 
+fun main() {
+    val foo = Foo("bar1")
+    foo.barPrint()
+    foo.run { println("bar: $bar | foo_bar: $foo_bar") }
+}
+
 interface IFoo {
     val bar: String
+    val foo_bar: String
+    get() = "foo$bar"
     fun barPrint()
 }
 
