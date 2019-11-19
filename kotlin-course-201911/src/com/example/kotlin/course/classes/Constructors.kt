3300c22ef10c590d2c108e2a42584980e42d2ab7
João Luzio <joao.luzio@gmail.com>
2019-11-19 19:26:44 +0000

    Kotlin Course

--- kotlin-course-201911/src/com/example/kotlin/course/classes/Constructors.kt
+++ kotlin-course-201911/src/com/example/kotlin/course/classes/Constructors.kt
@@ -54,10 +54,21 @@ class Employee6 {
     }
 }
 
-class User(val name: String) {
+open class User(val name: String) {
     constructor(firstName: String, surname: String): this("$firstName $surname")
 }
-
+// secondary constructor using super primary constructor
+class User2(name: String): User(name) {
+    constructor(firstName: String, surname: String): this("$firstName $surname") {
+    }
+}
+// usage of super secondary constructor allowed if no secondary constructor exists
+class User3(firstName: String, surname: String): User(firstName, surname)
+// to define secondary constructor, don't declare primary constructor
+class User4: User {
+    constructor(firstName: String, surname: String): super(firstName, surname) {
+    }
+}
 
 fun main() {
     println(Employee1("e1").name)
