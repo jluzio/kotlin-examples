eeeac7718cf4c1f6069bd31f07cc884a7a99e493
João Luzio <joao.luzio@gmail.com>
2019-11-29 18:41:27 +0000

    Kotlin Course

---
+++ kotlin-course-201911/src/com/example/kotlin/course/collections/Cars.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin.course.collections
+
+class NonDestructurableCar(val color: String, val model: String, val year: Int)
+class DestructurableCar(val color: String, val model: String, val year: Int) {
+    operator fun component1() = color
+    operator fun component2() = model
+    operator fun component3() = year
+}
+data class DataCar(val color: String, val model: String, val year: Int)
---
+++ kotlin-course-201911/src/com/example/kotlin/course/collections/ListMethods.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin.course.collections
+
+fun main() {
+    val stringList = mutableListOf("spring", "summer", "fall", "stuff", "red")
+    val colorList = mutableListOf("black", "red", "green")
+    val intList = listOf(3, 5, 2, 7)
+
+    println(intList.getOrNull(5))
+    val retGetOrElse = intList.getOrElse(5, { "no value found at index $it" })
+    println(retGetOrElse)
+
+    println(colorList.last())
+
+    val reversedList = colorList.reversed()
+    println(reversedList)
+    val reversedViewOfList = colorList.asReversed()
+    println(reversedViewOfList)
+
+    colorList += "blue"
+    println(reversedList)
+    println(reversedViewOfList)
+
+    println(colorList.max())
+
+    println(colorList + stringList)
+
+    // no dups
+    println(colorList.union(stringList))
+    println((colorList + stringList).distinct())
+
+}
