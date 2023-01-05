298427af760bf25b4be8b057ad96bdb66d0d262a
João Luzio <joao.luzio@gmail.com>
2023-01-05 17:47:50 +0000

    Kotlin 1.8.x

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
-//	id("org.jetbrains.kotlin.plugin.allopen") version "1.7.21"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+//	id("org.jetbrains.kotlin.plugin.allopen") version "1.8.0"
+	kotlin("jvm") version "1.8.0"
+	kotlin("plugin.spring") version "1.8.0"
 }
 
 group = "com.example.kotlin"
@@ -25,7 +25,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
 	implementation("com.google.guava:guava:31.1-jre")
