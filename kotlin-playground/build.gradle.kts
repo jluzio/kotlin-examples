73bb99a92a8a0c72459a210509e6ecb81c4ef39e
João Luzio <joao.luzio@gmail.com>
2023-01-05 17:05:52 +0000

    Spring Boot 3.x | Kotlin 1.7.x

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0"
+	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
 //	id("org.jetbrains.kotlin.plugin.allopen") version "1.7.21"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "org.example.kotlin"
@@ -31,7 +31,7 @@ dependencies {
 	implementation("com.google.guava:guava:31.1-jre")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.mockito:mockito-core:4.8.0")
+	testImplementation("org.mockito:mockito-core:4.11.0")
 	testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 }
 
