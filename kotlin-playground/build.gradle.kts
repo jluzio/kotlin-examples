0ec799d1dcae32e786267708b8f9ccbb7104e1c5
João Luzio <joao.luzio@gmail.com>
2023-05-02 18:06:59 +0100

    Gradle and Kotlin version

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.5"
 	id("io.spring.dependency-management") version "1.1.0"
-//	id("org.jetbrains.kotlin.plugin.allopen") version "1.8.10"
-	kotlin("jvm") version "1.8.10"
-	kotlin("plugin.spring") version "1.8.10"
+//	id("org.jetbrains.kotlin.plugin.allopen") version "1.8.20"
+	kotlin("jvm") version "1.8.20"
+	kotlin("plugin.spring") version "1.8.20"
 
 	// Lombok
 	id("io.freefair.lombok") version "8.0.1"
-	kotlin("plugin.lombok") version "1.8.10"
+	kotlin("plugin.lombok") version "1.8.20"
 }
 
 group = "com.example.kotlin"
