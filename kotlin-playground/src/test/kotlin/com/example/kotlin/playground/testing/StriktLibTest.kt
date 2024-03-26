68a8fdd7b732f7110ca825866fdb48250083342e
João Luzio <joao.luzio@gmail.com>
2024-03-26 14:20:37 +0000

    Examples

--- kotlin-playground/src/test/kotlin/com/example/kotlin/playground/testing/StriktLibTest.kt
+++ kotlin-playground/src/test/kotlin/com/example/kotlin/playground/testing/StriktLibTest.kt
@@ -1,13 +1,16 @@
 package com.example.kotlin.playground.testing
 
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.slf4j.LoggerFactory
 import strikt.api.expect
 import strikt.api.expectThat
 import strikt.assertions.*
 import kotlin.test.Test
 
+
 class StriktLibTest {
 
+  val log = LoggerFactory.getLogger(this::class.java)
   val testSample = BasicTestSample()
 
   @Test
@@ -25,14 +28,32 @@ class StriktLibTest {
     expect {
       that("fnord")
         .isA<String>()
-        .hasLength(1)
-        .isUpperCase()
+        .hasLength(5)
+        .isLowerCase()
       that(1L) {
-        isLessThan(1L).isA<Int>()
-        isGreaterThan(1L)
+        isLessThanOrEqualTo(1L).isA<Long>()
+        isGreaterThanOrEqualTo(1L)
       }
     }
+  }
 
+  @Test
+  fun checkErrorReportingFormat() {
+    try {
+      expect {
+        that("fnord")
+          .isA<String>()
+          .hasLength(1)
+          .isUpperCase()
+        that(1L) {
+          isLessThan(1L).isA<Int>()
+          isGreaterThan(1L)
+        }
+      }
+    } catch (t: Throwable) {
+      // encoding of message gets messed up compared to when the exception is handled by the test framework
+      System.err.println(t.message)
+    }
   }
 
 }
